8828621342ae10aef04b481d599421219e91387d
Kenneth C. Salanga <kcs132@kill.cs.rutgers.edu>
2022-04-05 21:42:52 -0400

    multi test with two threads, two pages

--- benchmark/multi_test.c
+++ benchmark/multi_test.c
@@ -1,33 +1,38 @@
 #include "../my_vm.h"
 #include <time.h>
-#define num_threads 15
+#define num_threads 2
 
 void *pointers[num_threads];
 int ids[num_threads];
 pthread_t threads[num_threads];
-int alloc_size = 10000;
+int alloc_size = 10; // 000
 int matrix_size = 5;
 
-void *alloc_mem(void *id_arg) {
+void *alloc_mem(void *id_arg)
+{
     int id = *((int *)id_arg);
     pointers[id] = t_malloc(alloc_size);
     return NULL;
 }
 
-void *put_mem(void *id_arg) {
+void *put_mem(void *id_arg)
+{
     int val = 1;
     void *va_pointer = pointers[*((int *)id_arg)];
-    for (int i = 0; i < matrix_size; i++) {
-        for (int j = 0; j < matrix_size; j++) {
+    for (int i = 0; i < matrix_size; i++)
+    {
+        for (int j = 0; j < matrix_size; j++)
+        {
             int address_a = (unsigned int)va_pointer + ((i * matrix_size * sizeof(int))) + (j * sizeof(int));
             put_value((void *)address_a, &val, sizeof(int));
-	    //val++;
+            // val++;
         }
     }
     return NULL;
 }
 
-void *mat_mem(void *id_arg) {
+void *mat_mem(void *id_arg)
+{
     int i = *((int *)id_arg);
     void *a = pointers[i];
     void *b = pointers[i + 1];
@@ -36,12 +41,14 @@ void *mat_mem(void *id_arg) {
     return NULL;
 }
 
-void *free_mem(void *id_arg) {
+void *free_mem(void *id_arg)
+{
     int id = *((int *)id_arg);
     t_free(pointers[id], alloc_size);
 }
 
-int main() {
+int main()
+{
     srand(time(NULL));
 
     for (int i = 0; i < num_threads; i++)
@@ -49,7 +56,7 @@ int main() {
 
     for (int i = 0; i < num_threads; i++)
         pthread_create(&threads[i], NULL, alloc_mem, (void *)&ids[i]);
-    
+
     for (int i = 0; i < num_threads; i++)
         pthread_join(threads[i], NULL);
 
@@ -57,48 +64,53 @@ int main() {
     for (int i = 0; i < num_threads; i++)
         printf("%x ", (int)pointers[i]);
     printf("\n");
-    
-    printf("initializing some of the memory by in multiple threads\n");
-    for (int i = 0; i < num_threads; i++)
-        pthread_create(&threads[i], NULL, put_mem, (void *)&ids[i]);
 
-    for (int i = 0; i < num_threads; i++)
-        pthread_join(threads[i], NULL);
+    // printf("initializing some of the memory by in multiple threads\n");
+    // for (int i = 0; i < num_threads; i++)
+    //     pthread_create(&threads[i], NULL, put_mem, (void *)&ids[i]);
 
-    printf("Randomly checking a thread allocation to see if everything worked correctly!\n");
-    int rand_id = rand() % num_threads;
-    void *a = pointers[rand_id];
-    int val = 0;
-    for (int i = 0; i < matrix_size; i++) {
-        for (int j = 0; j < matrix_size; j++) {
-            int address_a = (unsigned int)a + ((i * matrix_size * sizeof(int))) + (j * sizeof(int));
-            get_value((void *)address_a, &val, sizeof(int));
-            printf("%d ", val);
-        }
-        printf("\n");
-    }
+    // for (int i = 0; i < num_threads; i++)
+    //     pthread_join(threads[i], NULL);
 
-    printf("Performing matrix multiplications in multiple threads threads!\n");
+    // printf("Randomly checking a thread allocation to see if everything worked correctly!\n");
+    // int rand_id = rand() % num_threads;
+    // void *a = pointers[rand_id];
+    // int val = 0;
+    // for (int i = 0; i < matrix_size; i++) {
+    //     for (int j = 0; j < matrix_size; j++) {
+    //         int address_a = (unsigned int)a + ((i * matrix_size * sizeof(int))) + (j * sizeof(int));
+    //         get_value((void *)address_a, &val, sizeof(int));
+    //         printf("%d ", val);
+    //     }
+    //     printf("\n");
+    // }
 
-    for (int i = 0; i < num_threads; i+=3)
-        pthread_create(&threads[i], NULL, mat_mem, (void *)&ids[i]);
+    // printf("Performing matrix multiplications in multiple threads threads!\n");
 
-    for (int i = 0; i < num_threads; i+=3)
-        pthread_join(threads[i], NULL);
+    // for (int i = 0; i < num_threads; i+=3)
+    //     pthread_create(&threads[i], NULL, mat_mem, (void *)&ids[i]);
 
-    printf("Randomly checking a thread allocation to see if everything worked correctly!\n");
-    rand_id = (((rand() % (num_threads/3)) + 1) * 3) - 1;
-    a = pointers[rand_id];
-    val = 0;
+    // for (int i = 0; i < num_threads; i+=3)
+    //     pthread_join(threads[i], NULL);
 
-    for (int i = 0; i < matrix_size; i++) {
-        for (int j = 0; j < matrix_size; j++) {
-            int address_a = (unsigned int)a + ((i * matrix_size * sizeof(int))) + (j * sizeof(int));
-            get_value((void *)address_a, &val, sizeof(int));
-            printf("%d ", val);
-        }
-        printf("\n");
-    }
+    // printf("Randomly checking a thread allocation to see if everything worked correctly!\n");
+    // rand_id = (((rand() % (num_threads/3)) + 1) * 3) - 1;
+    // a = pointers[rand_id];
+    // val = 0;
+
+    // for (int i = 0; i < matrix_size; i++) {
+    //     for (int j = 0; j < matrix_size; j++) {
+    //         int address_a = (unsigned int)a + ((i * matrix_size * sizeof(int))) + (j * sizeof(int));
+    //         get_value((void *)address_a, &val, sizeof(int));
+    //         printf("%d ", val);
+    //     }
+    //     printf("\n");
+    // }
+
+    // // TODO: Delete later
+    // printf("Printing Allocated Bitmap!\n");
+    // printBitmaps();
+
     int old = (int)pointers[0];
     printf("Gonna free everything in multiple threads!\n");
     // ufree(pointers[0], alloc_size);
@@ -109,17 +121,31 @@ int main() {
     for (int i = 0; i < num_threads; i++)
         pthread_join(threads[i], NULL);
 
+    // // TODO: Delete later
+    // printf("Printing Free Bitmap!\n");
+    // printBitmaps();
+
     void *temp = (void *)1;
     int flag = 0;
-    while (temp != NULL) {
+    int count = 0;
+    while (temp != NULL)
+    {
         temp = t_malloc(10);
-        if ((int)temp == old) {
+        fprintf(stdout, "%x ", (int)temp);
+        if (count == num_threads)
+        {
+            break;
+        }
+        if ((int)temp == old)
+        {
             printf("Free Worked!\n");
             flag = 1;
             break;
         }
+        count++;
     }
-    if (!flag) {
+    if (!flag)
+    {
         printf("Some Problem with free!\n");
     }
 }
